26b899013a2c0f7760cd18acb6ce5068e4520c32
HYDRAISHI <rastogiabhi123@gmail.com>
2021-08-17 21:09:10 +0530

    registered names of the players

--- js/Game.js
+++ js/Game.js
@@ -3,7 +3,7 @@ class Game {
   
   getState(){
     var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
+      gameStateRef.on("value",function(data){
        gameState = data.val();
     })
    
@@ -15,12 +15,25 @@ class Game {
     });
   }
 
-  start(){
+ async start(){
     if(gameState === 0){
-      //player = new Player();
-      //player.getCount();
+      player = new Player(); 
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
     }
+
+
+  }
+
+  play(){
+   form.hide();
+   textSize(30);
+   fill("red");
+   text("The Game Is About To Start", 10, 100);   
   }
 }
